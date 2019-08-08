09e789cf53db0c6092a58fd6f096bdef4a9f8288
865142078@qq.com <865142078@qq.com>
2019-08-08 18:29:30 +0800

    列表

--- pages/aboutme/aboutme.js
+++ pages/aboutme/aboutme.js
@@ -1,18 +1,53 @@
 // pages/aboutme/aboutme.js
+var app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    loading: true, // 显示等待框
+    userToken:"",
+    info:{},
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    //从本地缓存拿userToken
+    wx.getStorageSync(key)({
+      key: 'userToken',
+      success: function (res) {
+        this.userToken = res.data
+      },
+      fail: function (res) {
+        wx.showToast({
+          title: '登录状态失效，请重新登录',
+          icon: "none",
+          duration: 1500
+        })
+        wx.navigateTo({
+          url: '../login/login',
+        })
+      },
+    })
+    //userToken为空时返回登录界面
+    if(this.userToken == ""){
+      wx.showToast({
+        title: '登录状态失效，请重新登录',
+        icon: "none",
+        duration: 1500
+      })
+      wx.navigateTo({
+        url: '../login/login',
+      })
+    }else{
+      wx.request({//获取我的列表
+        url: app.globalData.serverIp + "/api/v2/aashop/mylist",
+        
+      })
+    }
   },
 
   /**
